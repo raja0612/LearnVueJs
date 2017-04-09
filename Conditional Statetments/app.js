0c206a8cb3a3c4411b122130790d8c05d2467925
raja0612 <rajashekhar.ramayampeta@gmail.com>
2017-04-09 11:02:04 -0400

    added condtional and looping statements

---
+++ Conditional Statetments/app.js
@@ -0,0 +1,10 @@
+new Vue({
+
+   el: "#app",
+
+   data:{
+   
+      show: true
+   }
+
+});
---
+++ Looping Statements/app.js
@@ -0,0 +1,21 @@
+new Vue({
+
+    el: "#app",
+
+    data: {
+
+    	ingredients : ['Milk','Meat','Peanut Butter'],
+
+    	persons  : [
+
+          {name:"Jack",age:26,profession:"Software Developer"},
+
+          {name:"Raj",age:26,profession:"Systems Analyst"},
+
+          {name:"Mahesh",age:26,profession:"Chemical Enginner"},
+
+          {name:"Vishu",age:28,profession:"Full Stack Developer"}
+    	]
+    }
+
+});
